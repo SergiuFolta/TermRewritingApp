fee769166efbce7f7a557d181f839734b8474853
SergiuFolta <sergiu.folta01@e-uvt.ro>
2024-04-08 13:39:08 +0300

    Added functions.html

--- website/views.py
+++ website/views.py
@@ -24,3 +24,16 @@ def home():
     functions = session["functions"] if "functions" in session else {}
     variables = session["variables"] if "variables" in session else set([])
     return render_template("home.html", input_string = input_string, functions = functions, variables = variables, tree = tree)
+
+@views.route('/functions', methods=['GET', 'POST'])
+def functions():
+    input_string = session["input_string"] if "input_string" in session else ""
+    
+    if request.method == 'POST':
+        pass
+            
+    
+    functions = session["functions"] if "functions" in session else {}
+    variables = session["variables"] if "variables" in session else set([])
+    return render_template("functions.html", functions = functions)
+

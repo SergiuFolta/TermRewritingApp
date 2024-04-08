82972aa90ddb928484eab1bde4574bb3ad640f42
SergiuFolta <sergiu.folta01@e-uvt.ro>
2024-04-08 16:41:53 +0300

    Redid everything

--- website/views.py
+++ website/views.py
@@ -1,29 +1,33 @@
 import os
-from flask import Blueprint, render_template, request, session, current_app as app
+from flask import Blueprint, render_template, redirect, url_for, request, session, current_app as app
 from website.backend.backend import *
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    input_string = session["input_string"] if "input_string" in session else ""
-    tree = ""
-    
-    if request.method == 'POST':
-        input_string = request.form.get('term')
-        session["input_string"] = input_string
-        
-        if request.form.get('detect'):
-            LoadLanguage(input_string)
-            
-        if request.form.get('create'):
-            head = CreateTree(input_string)
-            tree = ChangeTreeToList(head)
-            
-    
     functions = session["functions"] if "functions" in session else {}
     variables = session["variables"] if "variables" in session else set([])
-    return render_template("home.html", input_string = input_string, functions = functions, variables = variables, tree = tree)
+    term_name = session["term_name"] if "term_name" in session else ""
+    term_string = session["term_string"] if "term_string" in session else ""
+    term_dictionary = session["terms"] if "terms" in session else {}
+    terms = term_dictionary.keys()
+    tree = ""
+    
+    if request.method == 'POST':  
+        if request.form.get('functions'):
+            return redirect(url_for('views.functions', functions = functions))
+        
+        if request.form.get('variables'):
+            return redirect(url_for('views.variables', variables = variables))
+        
+        if request.form.get('terms'):
+            return redirect(url_for('views.terms', functions = functions, variables = variables, terms = terms, tree = tree))
+        
+        if request.form.get('create'):
+            return redirect(url_for('views.createterm', term_name = term_name, term_string = term_string, functions = functions, variables = variables))
+            
+    return render_template("home.html")
 
 @views.route('/functions', methods=['GET', 'POST'])
 def functions():
@@ -47,6 +51,9 @@ def functions():
             if  request.form.get('delete' + str(i + 1)):
                 DeleteFunction(f)
                 break
+        
+        if request.form.get('home'):
+            return redirect(url_for('views.home'))
     
     return render_template("functions.html", functions = functions)
 
@@ -55,6 +62,9 @@ def variables():
     variables = set(session["variables"]) if "variables" in session else set([])
     
     if request.method == 'POST':
+        if request.form.get('home'):
+            return redirect(url_for('views.home'))
+        
         if request.form.get('addnew'):
             var_name = request.form.get('varnew')
             AddVariable(var_name)
@@ -72,3 +82,49 @@ def variables():
         variables = set(session["variables"])
     
     return render_template("variables.html", variables = variables)
+
+@views.route('/terms', methods=['GET', 'POST'])
+def terms():
+    functions = session["functions"] if "functions" in session else {}
+    variables = session["variables"] if "variables" in session else set([])
+    term_dictionary = session["terms"] if "terms" in session else {}
+    terms = term_dictionary.keys()
+    tree = ""
+    
+    if request.method == 'POST':    
+        if request.form.get('display'):
+            term_name = request.form.get('term')
+            tree = LoadTerm(term_name)
+            if tree is not None:
+                tree = tree[1]
+        
+        if request.form.get('home'):
+            return redirect(url_for('views.home'))
+            
+    return render_template("terms.html", functions = functions, variables = variables, terms = terms, tree = tree)
+
+@views.route('/createterm', methods=['GET', 'POST'])
+def createterm():
+    term_name = session["term_name"] if "term_name" in session else ""
+    term_string = session["term_string"] if "term_string" in session else ""
+    
+    if request.method == 'POST':
+        term_name = request.form.get('name')
+        term_string = request.form.get('string')
+        session["term_name"] = term_name
+        session["term_string"] = term_string
+        
+        if request.form.get('detect'):
+            LoadLanguage(term_string)
+            
+        if request.form.get('save'):
+            head = CreateTree(term_string)
+            tree = ChangeTreeToList(head)
+            SaveTerm(tree, term_string, term_name)
+            
+        if request.form.get('home'):
+            return redirect(url_for('views.home'))
+    
+    functions = session["functions"] if "functions" in session else {}
+    variables = session["variables"] if "variables" in session else set([])
+    return render_template("createterm.html", term_name = term_name, term_string = term_string, functions = functions, variables = variables)
